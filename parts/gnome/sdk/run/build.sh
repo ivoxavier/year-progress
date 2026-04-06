cce202a38793f11d97790f61b012b659074ceeee
ixsvf <ivofernandes12@gmail.com>
2026-04-06 23:08:40 +0100

    remove parts folder

--- parts/gnome/sdk/run/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-source /home/ivo-xavier/Documentos/GitHub/year-progress/parts/gnome/sdk/run/environment.sh
-set -x
-make -j"4" GPU_WRAPPER=gpu-2404-wrapper
-make -j"4" install GPU_WRAPPER=gpu-2404-wrapper DESTDIR="/home/ivo-xavier/Documentos/GitHub/year-progress/parts/gnome/sdk/install"
